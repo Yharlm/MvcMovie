c4d6a9a099872b592b18ba10d352dccbe5b070b2
Peshk0 <petar.tonev.pp25@pgmtplovdiv.info>
2025-07-04 11:32:17 +0300

    Finished tutorial

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -12,16 +12,23 @@ namespace MvcMovie.Models
 
 
 
-        [MaxLength(30)]
+        [StringLength(60, MinimumLength = 2)]
+        [Required]
         public string? Title { get; set; }
 
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string? Genre { get; set; }
         [Column(TypeName = "decimal(18, 2)")]
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
-
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
         public string? Rating { get; set; }
     }
 }
